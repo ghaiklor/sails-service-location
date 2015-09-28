5d44e8c6ef79b66e24bc4c39ebc161baca2898b3
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 18:55:41 +0300

    Rewrite YandexLocation with ES6

--- src/YandexLocation.js
+++ src/YandexLocation.js
@@ -1,36 +1,29 @@
-var geocoder = require('node-geocoder');
-var util = require('util');
-var BaseLocation = require('./BaseLocation');
+import geocoder from 'node-geocoder';
+import BaseLocation from './BaseLocation';
 
-util.inherits(YandexLocation, BaseLocation);
+export default class YandexLocation extends BaseLocation {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create GM instance
- * @constructor
- */
-function YandexLocation() {
-  BaseLocation.apply(this, arguments);
+    this.setProvider(geocoder('yandex', 'http', this.get()));
+  }
 
-  this.setProvider(geocoder('yandex', 'http', this.get()));
+  /**
+   * Geocode address and get latitude\longitude for it
+   * @param {String} address
+   * @returns {Promise}
+   */
+  geocode(address) {
+    return this.getProvider().geocode(address);
+  }
+
+  /**
+   * Reverse geocode to address
+   * @param {Number} latitude
+   * @param {Number} longitude
+   * @returns {Promise}
+   */
+  reverse(latitude, longitude) {
+    return this.getProvider().reverse({lat: latitude, lon: longitude});
+  }
 }
-
-/**
- * Geocode address and get latitude\longitude for it
- * @param {String} address
- * @returns {Promise}
- */
-YandexLocation.prototype.geocode = function (address) {
-  return this.getProvider().geocode(address);
-};
-
-/**
- * Reverse geocode to address
- * @param {Number} latitude
- * @param {Number} longitude
- * @returns {Promise}
- */
-YandexLocation.prototype.reverse = function (latitude, longitude) {
-  return this.getProvider().reverse({lat: latitude, lon: longitude});
-};
-
-module.exports = YandexLocation;
