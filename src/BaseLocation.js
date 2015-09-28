bf2ef683f900ae9a13451ab3883b22ea9c9d0135
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 17:45:01 +0300

    Rewrite BaseLocation with ES6

--- src/BaseLocation.js
+++ src/BaseLocation.js
@@ -1,57 +1,48 @@
-var _ = require('lodash');
+import _ from 'lodash';
 
-/**
- * Create base instance for location service
- * @param {Object} [_config]
- * @constructor
- */
-function BaseLocation(_config) {
-  this._config = {};
+export default class BaseLocation {
+  constructor(_config) {
+    this._config = {};
+    this._provider = {};
 
-  _.forOwn(_config, function (value, key) {
-    this.set(key, value);
-  }.bind(this));
+    _.assign(this._config, _config);
+  }
+
+  /**
+   * Get configuration value
+   * @param {String} [path]
+   * @returns {*}
+   */
+  get(path) {
+    return typeof path === 'undefined' ? this._config : _.get(this._config, path);
+  }
+
+  /**
+   * Set configuration value
+   * @param {String} path
+   * @param {*} value
+   * @returns {BaseLocation}
+   */
+  set(path, value) {
+    _.set(this._config, path, value);
+    return this;
+  }
+
+  /**
+   * Get location provider
+   * @returns {*}
+   */
+  getProvider() {
+    return this._provider;
+  }
+
+  /**
+   * Set new provider
+   * @param {*} provider
+   * @returns {BaseLocation}
+   */
+  setProvider(provider) {
+    this._provider = provider;
+    return this;
+  }
 }
-
-/**
- * Get configuration value
- * @param {String} [path]
- * @returns {*}
- */
-BaseLocation.prototype.get = function (path) {
-  return typeof path === 'undefined' ? this._config : _.get(this._config, path);
-};
-
-/**
- * Set configuration value
- * @param {String} path
- * @param {*} value
- * @returns {BaseLocation}
- */
-BaseLocation.prototype.set = function (path, value) {
-  _.set(this._config, path, value);
-  return this;
-};
-
-/**
- * Get location provider
- * @returns {*}
- */
-BaseLocation.prototype.getProvider = function () {
-  return this._provider;
-};
-
-/**
- * Set new provider
- * @param {*} provider
- * @returns {BaseLocation}
- */
-BaseLocation.prototype.setProvider = function (provider) {
-  this._provider = provider;
-  return this;
-};
-
-BaseLocation.prototype.geocode = _;
-BaseLocation.prototype.reverse = _;
-
-module.exports = BaseLocation;
