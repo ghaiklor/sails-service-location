747508e3194f62b37d63b7e3638d577168346656
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 18:54:01 +0300

    Rewrite TomTomLocation with ES6

--- src/TomTomLocation.js
+++ src/TomTomLocation.js
@@ -1,36 +1,29 @@
-var geocoder = require('node-geocoder');
-var util = require('util');
-var BaseLocation = require('./BaseLocation');
+import geocoder from 'node-geocoder';
+import BaseLocation from './BaseLocation';
 
-util.inherits(TomTomLocation, BaseLocation);
+export default class TomTomLocation extends BaseLocation {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create GM instance
- * @constructor
- */
-function TomTomLocation() {
-  BaseLocation.apply(this, arguments);
+    this.setProvider(geocoder('tomtom', 'http', this.get()));
+  }
 
-  this.setProvider(geocoder('tomtom', 'http', this.get()));
+  /**
+   * Geocode address and get latitude\longitude for it
+   * @param {String} address
+   * @returns {Promise}
+   */
+  geocode(address) {
+    return this.getProvider().geocode(address);
+  }
+
+  /**
+   * Reverse geocode to address
+   * @param {Number} latitude
+   * @param {Number} longitude
+   * @returns {Promise}
+   */
+  reverse(latitude, longitude) {
+    return this.getProvider().reverse({lat: latitude, lon: longitude});
+  }
 }
-
-/**
- * Geocode address and get latitude\longitude for it
- * @param {String} address
- * @returns {Promise}
- */
-TomTomLocation.prototype.geocode = function (address) {
-  return this.getProvider().geocode(address);
-};
-
-/**
- * Reverse geocode to address
- * @param {Number} latitude
- * @param {Number} longitude
- * @returns {Promise}
- */
-TomTomLocation.prototype.reverse = function (latitude, longitude) {
-  return this.getProvider().reverse({lat: latitude, lon: longitude});
-};
-
-module.exports = TomTomLocation;
