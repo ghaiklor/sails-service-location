0532c0014c2dff269da3b1df2c376cffa683247c
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 18:53:17 +0300

    Rewrite SmartyStreetsLocation with ES6

--- src/SmartyStreetsLocation.js
+++ src/SmartyStreetsLocation.js
@@ -1,36 +1,29 @@
-var geocoder = require('node-geocoder');
-var util = require('util');
-var BaseLocation = require('./BaseLocation');
+import geocoder from 'node-geocoder';
+import BaseLocation from './BaseLocation';
 
-util.inherits(SmartyStreetsLocation, BaseLocation);
+export default class SmartyStreetsLocation extends BaseLocation {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create GM instance
- * @constructor
- */
-function SmartyStreetsLocation() {
-  BaseLocation.apply(this, arguments);
+    this.setProvider(geocoder('smartystreets', 'http', this.get()));
+  }
 
-  this.setProvider(geocoder('smartystreets', 'http', this.get()));
+  /**
+   * Geocode address and get latitude\longitude for it
+   * @param {String} address
+   * @returns {Promise}
+   */
+  geocode(address) {
+    return this.getProvider().geocode(address);
+  }
+
+  /**
+   * Reverse geocode to address
+   * @param {Number} latitude
+   * @param {Number} longitude
+   * @returns {Promise}
+   */
+  reverse(latitude, longitude) {
+    return this.getProvider().reverse({lat: latitude, lon: longitude});
+  }
 }
-
-/**
- * Geocode address and get latitude\longitude for it
- * @param {String} address
- * @returns {Promise}
- */
-SmartyStreetsLocation.prototype.geocode = function (address) {
-  return this.getProvider().geocode(address);
-};
-
-/**
- * Reverse geocode to address
- * @param {Number} latitude
- * @param {Number} longitude
- * @returns {Promise}
- */
-SmartyStreetsLocation.prototype.reverse = function (latitude, longitude) {
-  return this.getProvider().reverse({lat: latitude, lon: longitude});
-};
-
-module.exports = SmartyStreetsLocation;
