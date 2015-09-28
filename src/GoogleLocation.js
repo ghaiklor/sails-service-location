e10c74cfae74938772e188570f339ce85c409ea3
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 18:49:51 +0300

    Rewrite GoogleLocation with ES6

--- src/GoogleLocation.js
+++ src/GoogleLocation.js
@@ -1,36 +1,29 @@
-var geocoder = require('node-geocoder');
-var util = require('util');
-var BaseLocation = require('./BaseLocation');
+import geocoder from 'node-geocoder';
+import BaseLocation from './BaseLocation';
 
-util.inherits(GoogleLocation, BaseLocation);
+export default class GoogleLocation extends BaseLocation {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create GM instance
- * @constructor
- */
-function GoogleLocation() {
-  BaseLocation.apply(this, arguments);
+    this.setProvider(geocoder('google', this.get('apiKey') ? 'https' : 'http', this.get()));
+  }
 
-  this.setProvider(geocoder('google', this.get('apiKey') ? 'https' : 'http', this.get()));
+  /**
+   * Geocode address and get latitude\longitude for it
+   * @param {String} address
+   * @returns {Promise}
+   */
+  geocode(address) {
+    return this.getProvider().geocode(address);
+  }
+
+  /**
+   * Reverse geocode to address
+   * @param {Number} latitude
+   * @param {Number} longitude
+   * @returns {Promise}
+   */
+  reverse(latitude, longitude) {
+    return this.getProvider().reverse({lat: latitude, lon: longitude});
+  }
 }
-
-/**
- * Geocode address and get latitude\longitude for it
- * @param {String} address
- * @returns {Promise}
- */
-GoogleLocation.prototype.geocode = function (address) {
-  return this.getProvider().geocode(address);
-};
-
-/**
- * Reverse geocode to address
- * @param {Number} latitude
- * @param {Number} longitude
- * @returns {Promise}
- */
-GoogleLocation.prototype.reverse = function (latitude, longitude) {
-  return this.getProvider().reverse({lat: latitude, lon: longitude});
-};
-
-module.exports = GoogleLocation;
